ddcc00d78bb773be986cb935cb9fbb21362493a8
Oxana2332 <far_north@ro.ru>
2022-12-10 21:03:01 +0300

    correct tariffs, data.json  отдельным файлом, +key

--- tariffs/src/components/app/App.jsx
+++ tariffs/src/components/app/App.jsx
@@ -1,48 +1,25 @@
+import React, { useState } from 'react';
 import './app.css';
+import data from '../../data/data.json';
 import Rate from '../rate/Rate.jsx';
 
-const tariffs = [{
-  price: "300",
-  speed: "10",
-  header: "blue-header",
-  section: "blue-section"
-}, {
-  price: "450",
-  speed: "50",
-  header: "green-header",
-  section: "green-section"
-}, {
-  price: "550",
-  speed: "100",
-  header: "red-header",
-  section: "red-section",
-}, {
-  price: "1000",
-  speed: "200",
-  header: "black-header",
-  section: "black-section"
-}];
-
 function App() {
+const [count,setCount] = useState(0) 
 
-  return ( <div className = "App" > {
-      tariffs.map((rate) =>
-        <
-        Rate price = {
-          rate.price
-        }
-        speed = {
-          rate.speed
-        }
-        classHeader = {
-          rate.header
-        }
-        classSection = {
-          rate.section
-        }
-        isSelected={rate.isSelected}
+  return ( 
+  <div className = "App" > 
+  {data.map(rate =>(
+        <Rate 
+        key={rate.id}
+        price = {rate.price}
+        speed = {rate.speed}
+        classHeader = {rate.header}
+        classSection = {rate.section}
+        isSelected={rate.id === count}
+        setCount = {setCount}
+        id = {rate.id}
         />
-      )
+      ))
     } </div>
   );
 }
--- tariffs/src/components/rate/Rate.jsx
+++ tariffs/src/components/rate/Rate.jsx
@@ -1,22 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './rate.css';
 
-export default function Rate(props) {
+export default function Rate({price,speed,classHeader,classSection,isSelected,setCount,id}) {
 
-    const [selected, setSelected] = useState(false);
-    const handleChange = () => {
-        setSelected(!selected);
+    const classChange = isSelected ? "rate-card_selected" : "rate-card"
+
+    function getValueId(){
+        setCount(id)
     }
 
     return (
-        <div onClick={handleChange} className={selected ? "rate-card_selected" : "rate-card"}>
-            <header className={`rate-header ${props.classHeader}`}>Безлимитный {props.price}</header>
-            <section className={`rate-section ${props.classSection}`}>
+        <div className={classChange} onClick={getValueId} >
+            <header className={`rate-header ${classHeader}`}>Безлимитный {price}</header>
+            <section className={`rate-section ${classSection}`}>
                 <span className='rate-section_top'>руб</span>
-                <span className='rate-price'>{props.price}</span>
+                <span className='rate-price'>{price}</span>
                 <span className='rate-section_bottom'> /мес</span>
             </section>
-            <p className='rate-speed'>до {props.speed} Мбит/сек</p>
+            <p className='rate-speed'>до {speed} Мбит/сек</p>
             <footer className='rate-footer'>Объём включённого трафика не ограничен</footer>
         </div>
     )
